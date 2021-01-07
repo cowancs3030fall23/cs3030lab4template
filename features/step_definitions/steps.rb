cfa78034fc4cc2b814fef097c5194568e7325eb0
Ted Cowan <tcowan@my321.net>
2021-01-07 10:40:45 -0700

    Removed puts
    Change all occurrences of puts to log.

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,19 +1,19 @@
 Given /^OUTPUT is printed/ do
     stdoutOutput = all_commands.map { |c| c.stdout }.join("\n").strip
     if stdoutOutput != ""
-        puts "STDOUT>>>"
-        puts stdoutOutput
-        puts "<<<STDOUT"
+        log "STDOUT>>>"
+        log stdoutOutput
+        log "<<<STDOUT"
     else
-        puts "STDOUT is EMPTY"
+        log "STDOUT is EMPTY"
     end
     stderrOutput = all_commands.map { |c| c.stderr }.join("\n").strip
     if stderrOutput != ""
-        puts "STDERR>>>"
-        puts stderrOutput
-        puts "<<<STDERR"
+        log "STDERR>>>"
+        log stderrOutput
+        log "<<<STDERR"
     else
-        puts "STDERR is EMPTY"
+        log "STDERR is EMPTY"
     end
 end
 
@@ -30,7 +30,7 @@ Given /^I type a random degree fahrenheit between (.*) and (.*)$/ do |min, max|
 	@randomCelsius = (@randomFahrenheit - 32.0) * (5.0/9.0)
 	@randomFahrenheit = sprintf('%.1f', @randomFahrenheit)
 	@randomCelsius = sprintf('%.1f', @randomCelsius)
-	puts "Random #{@randomFahrenheit} degrees Fahrenheit = #{@randomCelsius} degrees Celsius"
+	log "Random #{@randomFahrenheit} degrees Fahrenheit = #{@randomCelsius} degrees Celsius"
 	step "I type \"#{@randomFahrenheit}\""
 end
 
@@ -44,7 +44,7 @@ Given /^I type a random degree celsius between (.*) and (.*)$/ do |min, max|
 	@randomFahrenheit = (9.0/5.0) * @randomCelsius + 32.0
 	@randomFahrenheit = sprintf('%.1f', @randomFahrenheit)
 	@randomCelsius = sprintf('%.1f', @randomCelsius)
-	puts "Random #{@randomCelsius} degrees Celsius = #{@randomFahrenheit} degrees Fahrenheit"
+	log "Random #{@randomCelsius} degrees Celsius = #{@randomFahrenheit} degrees Fahrenheit"
 	step "I type \"#{@randomCelsius}\""
 end
 
@@ -62,19 +62,19 @@ Given /^large numbers are punctuated with commas/ do
 	output.delete_at(0)
 	output.delete_at(0)
 	if users.length != output.length
-		puts "Your output:"
+		log "Your output:"
 		for i in 1..savedOutput.length
-			puts "Line #{i}: #{savedOutput[i-1]}"
+			log "Line #{i}: #{savedOutput[i-1]}"
 		end
-		puts
+		log
 		raise("#{output.length} rows of data (less the header) found in output but #{users.length} expected")
 	end
 	for i in 0..output.length-1
 		commaCount = users[i].count.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
 		if output[i].match(/#{commaCount}[ ]+#{users[i].userid}/) == nil
-			puts "Your output:"
+			log "Your output:"
 			for n in 1..savedOutput.length
-				puts "Line #{n}: #{savedOutput[n-1]}"
+				log "Line #{n}: #{savedOutput[n-1]}"
 			end
 			raise("'#{output[i]}' should have commas like this: '#{commaCount} #{users[i].userid}'")
 		end
@@ -82,7 +82,7 @@ Given /^large numbers are punctuated with commas/ do
 end
 
 Given /^(.*) points are awarded/ do |points|
-	#puts "#{points} points are now awarded!!!"
+	#log "#{points} points are now awarded!!!"
 	$total_points += points.to_i
 end
 
@@ -90,7 +90,7 @@ Given /^timeout is increased by (.*) seconds$/ do |seconds|
 	if @aruba_timeout_seconds  
 		@aruba_timeout_seconds += seconds.to_i
 	else
-		puts "aruba_timeout_seconds is NIL!"
+		log "aruba_timeout_seconds is NIL!"
 	end
 end
 
@@ -98,7 +98,7 @@ Given /^timeout is decreased by (.*) seconds$/ do |seconds|
 	if @aruba_timeout_seconds
 		@aruba_timeout_seconds -= seconds.to_i
 	else
-		puts "aruba_timeout_seconds is NIL!"
+		log "aruba_timeout_seconds is NIL!"
 	end
 end
 
