e5b46374e7e6e071a2bbc1ffcd17acfc5b1bddd0
Ted Cowan <tcowan@my321.net>
2021-01-07 19:42:47 -0700

    Undid puts to log in grading.rb.

--- features/support/grading.rb
+++ features/support/grading.rb
@@ -1,19 +1,19 @@
 $total_points = 0
 
 Before do
-	# log Dir.pwd
+	# puts Dir.pwd
 	ENV['LIBC_FATAL_STDERR_'] = "1"
 end
 
 at_exit do
-	log "A total of #{$total_points} points have been awarded."
+	puts "A total of #{$total_points} points have been awarded."
 	#running = `ps ef` 
 	running = `ps` 
 	#if running =~ /python.+lab4.temp/
 	if running =~ /temp/
-		log "*** one or more temp processes are STILL running"
-		#log running
-		log "*** Issuing \"killall temp\""
+		puts "*** one or more temp processes are STILL running"
+		#puts running
+		puts "*** Issuing \"killall temp\""
 		`killall temp`
 	end
 end
